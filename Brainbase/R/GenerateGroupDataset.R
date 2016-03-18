72a101b6b8d3730b8e8b8fb53d26157e6730a558
haixiao990 <haixiao990@aliyun.com>
2016-03-19 00:01:50 +0800

    GenerateGroupDataset
    Will complete it tomorrow

---
+++ Brainbase/R/GenerateGroupDataset.R
@@ -0,0 +1,6 @@
+setGeneric("GenerateGroupDataset", function(obj, template, ...) standardGeneric("GenerateGroupDataset"))
+
+setMethod("GenerateGroupDataset", signature("NIdata", "Template"), function(obj, template, verbose = TRUE){
+
+  
+})
