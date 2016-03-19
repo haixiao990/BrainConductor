8e16287bfd048f419c9fd0419952a2dab0215107
haixiao990 <haixiao990@aliyun.com>
2016-03-19 21:31:14 +0800

    Revert "GenerateGroupDataset"
    This reverts commit 72a101b6b8d3730b8e8b8fb53d26157e6730a558.

--- Brainbase/R/GenerateGroupDataset.R
+++
@@ -1,6 +0,0 @@
-setGeneric("GenerateGroupDataset", function(obj, template, ...) standardGeneric("GenerateGroupDataset"))
-
-setMethod("GenerateGroupDataset", signature("NIdata", "Template"), function(obj, template, verbose = TRUE){
-
-  
-})
