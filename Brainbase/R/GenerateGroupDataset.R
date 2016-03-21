76649eb0d649fbb2627d258b4cd2f73ba16f3d76
haixiao990 <haixiao990@aliyun.com>
2016-03-21 14:53:02 +0800

    a few changes
    a few changes in Generate Group Dataset

--- Brainbase/R/GenerateGroupDataset.R
+++ Brainbase/R/GenerateGroupDataset.R
@@ -2,13 +2,13 @@ setGeneric("GenerateGroupDataset", function(Path, SbjList, OutputPath, Mask, lb 
 #Maybe Mask and Template could also be in NIdata class format, with a specific description slot to indicate data, mask, or ROI template
 
 setMethod("GenerateGroupDataset", signature("character", "character", "character","NIData", "numeric", "numeric"), function(Path, SbjList, template, verbose = TRUE){
-#  num_Sbj <- length(SbjList)
-#  for (i in 1:num_Sbj) {
-#    filename[i] <- list.files(Path,SbjList[i])
-#  }
+  num_Sbj <- length(SbjList)
+  for (i in 1:num_Sbj) {
+    Sbjfilename[i] <- list.files(Path,SbjList[i])
+  }
   
     
-  filename <- paste0(Path,"/",SbjList) 
+  filename <- paste0(Path,"/",Sbjfilename) 
   for (i in 1:length(filename)) {
       filename[i] <- sub("\\.gz$", "", filename[i])
       parts <- strsplit(filename[i],"\\.")
@@ -16,13 +16,17 @@ setMethod("GenerateGroupDataset", signature("character", "character", "character
       filesuff <- parts[[1]][nparts] 
       filetype  <- switch(filesuff, 
                           Rdata = 'Rdata',
-                          rda   = 'Rdata',
+                          rda = 'Rdata',
                           "NIfile")
       if (filetype == 'Rdata'){
         Objname <- load(filename[i])          
+        if (length(Objname)!=1)
+          .warning('The Rdata file of ',SbjList[i],'includes not only one objects')
         Obj <- get(Objname)
+        if (class(obj)!='NIdata')
+          .warning('The Rdata file ')
       } else {
-        BCoRead(filename[i])
+        obj <- BCoRead(filename[i])
       }
       
       #write BCod
