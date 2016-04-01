10729e5bfcbacae41ac3f56cdfc277f69f295125
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-03-31 20:49:53 -0400

    Made more modifications
    Started to test the FSLR functions

--- Brainbase/R/BCoFSL.R
+++ Brainbase/R/BCoFSL.R
@@ -1,5 +1,19 @@
 BCoFSL <- function(func.call, conversion.list = NULL, output.file = NULL){
 
+  if(is.null(options("fsl.path"))){
+    if(length(Sys.getenv("FSLDIR")) == 0) {
+      stop("FSL options are not set")
+    } else {
+      options("fsl.path" = Sys.getenv("FSLDIR"))
+    }
+  }
+
+  if(is.null(options("fsl.outputtype"))) {
+    print("FSL option 'fsl.outputtype' set to 'NIFTI_GZ' by default")
+    options("fsl.outputtype" = "NIFTI_GZ")
+  }
+
+
   if(!is.null(converstion.list){
     #write to directory
     for(i in 1:length(converstion.list)){
